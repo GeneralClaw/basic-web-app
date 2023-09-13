0fdb5026a84995405635f369b36f4368d320e805
GeneralClaw <116990813+GeneralClaw@users.noreply.github.com>
2023-09-13 14:26:36 -0400

    Added andrewID feature to query processor

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -15,6 +15,12 @@ describe("QueryProcessor", () => {
             "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
             "English poet, playwright, and actor, widely regarded as the greatest " +
             "writer in the English language and the world's pre-eminent dramatist."
-          ));
+        ));
+    });  // <-- This was missing
+
+    test('should return andrewid', () => {
+        const query = "andrewid";
+        const response: string = QueryProcessor(query);
+        expect(response).toBe("My Andrew ID is dballuff");
     });
 });
